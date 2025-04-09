c677f0fb1cb707b84bdfa0e6350e4075cd1777c2
vhyrro <vhyrro@gmail.com>
2025-04-09 17:53:48 +0200

    feat: add devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A test harness for Lux compatibility.";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    lux.url = "github:nvim-neorocks/lux";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                inputs'.lux.packages.lux-cli
+                git
+                gnumake
+                cmake
+                gcc
+                bc
+              ];
+            };
+        };
+      };
+    };
+}
